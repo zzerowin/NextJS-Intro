a25a11b74af491828e817e98d0d38970e318709f
zzerowin <alaykr123@gmail.com>
2022-04-19 02:06:37 +0900

    Static Pre Rendering

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
-export default function Main() {
-  return 'hi';
+import {useState} from "react";
+
+export default function Home() {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div>
+      <h1>Hello {counter}</h1>
+      <button onClick={() => setCounter((prev) => prev + 1)}>+</button>
+    </div>
+  );
 }
