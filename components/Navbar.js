f8f66d1616d76fec6e3c504ea0561f5a637e1d87
zzerowin <alaykr123@gmail.com>
2022-04-19 20:48:34 +0900

    Styles JSX

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,5 @@
 import Link from 'next/link'
 import {useRouter} from "next/router";
-import styles from './Navbar.module.css';
 
 export default function Navbar() {
   const router = useRouter();
@@ -9,21 +8,22 @@ export default function Navbar() {
     <nav>
 
       <Link href='/'>
-        <a className={`${styles.link} ${
-          router.pathname === '/' ? styles.active : ''
-        }`}
-        >
-          Home
-        </a>
+        <a className={router.pathname === '/' ? 'active' : ''}>Home</a>
       </Link>
       <Link href='/about'>
-        <a className={[
-          styles.link, router.pathname === '/about' ? styles.active : '',
-        ].join(' ')}
-        >
-          About
-        </a>
+        <a className={router.pathname === '/about' ? 'active' : ''}>About</a>
       </Link>
+      <style jsx>{`
+        nav {
+        background-color: tomato;
+        }
+        a {
+        text-decoration: none;
+        }
+        .active {
+          color: yellow;
+        }
+      `}</style>
     </nav>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,12 @@ export default function Home() {
   return (
     <div>
       <Navbar />
-      <h1>Hello</h1>
+      <h1 className='active'>Hello</h1>
+      <style jsx>{`
+      a {
+      color: white;
+      }
+    `}</style>
     </div>
   );
 }
