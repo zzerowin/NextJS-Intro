4994093d7d8be2d9f45e2584b7ac2b5b36c9d598
zzerowin <alaykr123@gmail.com>
2022-04-20 18:01:05 +0900

    Movie Detail

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,65 @@
 import Seo from "../components/Seo";
-import {useEffect, useState} from "react";
+import Link from "next/link";
+import {useRouter} from "next/router";
 
-export default function Home({ results }) {
+export default function Home({results}) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push(
+      {
+        pathname: `/movies/${id}`,
+        query: {
+          title
+        },
+      },
+      `/movies/${id}`
+    );
+  };
 
   return (
     <div className="container">
-      <Seo title="Home" />
+      <Seo title="Home"/>
       {results?.map((movie) => (
-        <div className="movie" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+        <div onClick={() => onClick(movie.id, movie.original_title)} className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}/>
+          <h4>
+            <Link href={
+              {
+                pathname: `/movies/${movie.id}`,
+                query: {
+                  title: movie.original_title,
+                }
+              }}
+                as={`/movies/${movie.id}`}
+            >
+              <a>{movie.original_title}</a>
+            </Link>
+          </h4>
         </div>
       ))}
-      <style jsx>{` 
+      <style jsx>{`
         .container {
           display: grid;
           grid-template-columns: 1fr 1fr;
           padding: 20px;
           gap: 20px;
         }
+
         .movie {
           cursor: pointer;
         }
+
         .movie img {
           max-width: 100%;
           border-radius: 12px;
           transition: transform 0.2s ease-in-out;
           box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
         }
+
         .movie:hover img {
           transform: scale(1.05) translateY(-10px);
         }
+
         .movie h4 {
           font-size: 18px;
           text-align: center;
@@ -41,7 +70,7 @@ export default function Home({ results }) {
 }
 
 export async function getServerSideProps() {
-  const { results } = await (
+  const {results} = await (
     await fetch(`http://localhost:3000/api/movies`))
     .json();
 
