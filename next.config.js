106c62ead739d544c20db00e1dfbd0566e3425b6
zzerowin <alaykr123@gmail.com>
2022-04-20 19:15:26 +0900

    Catch All

--- next.config.js
+++ next.config.js
@@ -20,8 +20,8 @@ const nextConfig = {
       },
       {
         source: "/api/movies/:id",
-        destination: `https://api.themoviedb.org/3/movie/:id??api_key=${API_KEY}`,
-      }
+        destination: `https://api.themoviedb.org/3/movie/:id?api_key=${API_KEY}`,
+      },
     ];
   }
 };
--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,25 @@
-import Seo from "../components/Seo";
 import Link from "next/link";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
+import Seo from "../components/Seo";
 
-export default function Home({results}) {
+export default function Home({ results }) {
   const router = useRouter();
   const onClick = (id, title) => {
-    router.push(
-      {
-        pathname: `/movies/${id}`,
-        query: {
-          title
-        },
-      },
-      `/movies/${id}`
-    );
+    router.push(`/movies/${title}/${id}`);
   };
 
   return (
     <div className="container">
-      <Seo title="Home"/>
+      <Seo title="Home" />
       {results?.map((movie) => (
-        <div onClick={() => onClick(movie.id, movie.original_title)} className="movie" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}/>
+        <div
+          onClick={() => onClick(movie.id, movie.original_title)}
+          className="movie"
+          key={movie.id}
+        >
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
           <h4>
-            <Link href={
-              {
-                pathname: `/movies/${movie.id}`,
-                query: {
-                  title: movie.original_title,
-                }
-              }}
-                as={`/movies/${movie.id}`}
-            >
+            <Link href={`/movies/${movie.original_title}/${movie.id}`}>
               <a>{movie.original_title}</a>
             </Link>
           </h4>
@@ -44,22 +32,18 @@ export default function Home({results}) {
           padding: 20px;
           gap: 20px;
         }
-
         .movie {
           cursor: pointer;
         }
-
         .movie img {
           max-width: 100%;
           border-radius: 12px;
           transition: transform 0.2s ease-in-out;
           box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
         }
-
         .movie:hover img {
           transform: scale(1.05) translateY(-10px);
         }
-
         .movie h4 {
           font-size: 18px;
           text-align: center;
@@ -70,13 +54,12 @@ export default function Home({results}) {
 }
 
 export async function getServerSideProps() {
-  const {results} = await (
-    await fetch(`http://localhost:3000/api/movies`))
-    .json();
-
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
   return {
     props: {
       results,
-    }
-  }
+    },
+  };
 }
